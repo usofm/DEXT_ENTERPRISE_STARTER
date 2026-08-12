eb578278093210dc1cac9373e62e98357ee5b9c1
usofm <71825788+usofm@users.noreply.github.com>
2026-08-12 23:54:34 +0400

    Model accounts as native Dext Entity

--- src/Features/Accounts/Domain/Accounts.Models.pas
+++ src/Features/Accounts/Domain/Accounts.Models.pas
@@ -3,17 +3,40 @@ unit Accounts.Models;
 interface
 
 uses
-  Data.FmtBcd;
+  System.SysUtils,
+  Dext.Entity,
+  Dext.Core.SmartTypes;
 
 type
-  TAccount = record
-    Id: Int64;
-    Code: string;
-    Name: string;
-    Balance: TBcd;
-    CreatedAt: TDateTime;
-    UpdatedAt: TDateTime;
+  [Table('accounts')]
+  TAccount = class
+  private
+    FId: Int64Type;
+    FCode: StringType;
+    FName: StringType;
+    FBalance: FmtBcdType;
+    FCreatedAt: TDateTime;
+    FUpdatedAt: TDateTime;
+  public
+    [PK, AutoInc]
+    property Id: Int64Type read FId write FId;
+
+    [Required, MaxLength(30)]
+    property Code: StringType read FCode write FCode;
+
+    [Required, MaxLength(200)]
+    property Name: StringType read FName write FName;
+
+    [Required, Precision(28, 10)]
+    property Balance: FmtBcdType read FBalance write FBalance;
+
+    [CreatedAt]
+    property CreatedAt: TDateTime read FCreatedAt write FCreatedAt;
+
+    [UpdatedAt]
+    property UpdatedAt: TDateTime read FUpdatedAt write FUpdatedAt;
   end;
 
 implementation
+
 end.
