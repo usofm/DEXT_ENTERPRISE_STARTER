f054a7b33e599a59a2abf974899043abe75558cc
usofm <71825788+usofm@users.noreply.github.com>
2026-08-13 00:00:39 +0400

    Add update validation rules

--- src/Features/Accounts/Application/Accounts.Rules.pas
+++ src/Features/Accounts/Application/Accounts.Rules.pas
@@ -9,6 +9,7 @@ type
   TAccountRules = record
   public
     class procedure ValidateCreate(const ARequest: TCreateAccountRequest); static;
+    class procedure ValidateUpdate(const ARequest: TUpdateAccountRequest); static;
   end;
 
 implementation
@@ -21,6 +22,12 @@ begin
   if Trim(ARequest.Code) = '' then
     raise EArgumentException.Create('Account code is required');
 
+  if Trim(ARequest.Name) = '' then
+    raise EArgumentException.Create('Account name is required');
+end;
+
+class procedure TAccountRules.ValidateUpdate(const ARequest: TUpdateAccountRequest);
+begin
   if Trim(ARequest.Name) = '' then
     raise EArgumentException.Create('Account name is required');
 end;
